56cd57348444734cfa22c8c12de3b1e637fadd76
Igor <sergienko339@gmail.com>
2022-07-22 13:27:06 +0300

    adeed some endpoints

--- server.js
+++ server.js
@@ -4,8 +4,19 @@ const app = express();
 
 const defaultPort = 7777;
 
+app.get("/", (req, res) => {
+  console.log("DEFAULT GET RESPONSE");
+  res.status(200);
+});
+
+app.post("/", (req, res) => {
+  console.log("DEFAULT POST RESPONSE");
+  res.status(200);
+});
+
 app.post("/webhook", (req, res) => {
   console.log("RESPONSE", res);
+  res.status(200);
 });
 
 app.listen(defaultPort, () => console.log("Server is running on port"));
