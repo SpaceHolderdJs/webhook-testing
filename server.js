a0cd38a76d5d301f2ee265777e8d8a14dcc3e506
Igor <sergienko339@gmail.com>
2022-07-22 13:51:49 +0300

    added consoles

--- server.js
+++ server.js
@@ -7,11 +7,13 @@ const app = express();
 const defaultPort = process.env.PORT || 7777;
 
 app.get("/", (req, res) => {
+  console.log("req", req, res);
   console.log("DEFAULT GET RESPONSE");
   res.status(200);
 });
 
 app.post("/", (req, res) => {
+  console.log("req", req, res);
   console.log("DEFAULT POST RESPONSE");
   res.status(200);
 });
@@ -21,4 +23,6 @@ app.post("/webhook", (req, res) => {
   res.status(200);
 });
 
-app.listen(defaultPort, () => console.log("Server is running on port"));
+app.listen(defaultPort, () =>
+  console.log(`Server is running on port ${defaultPort}`)
+);
